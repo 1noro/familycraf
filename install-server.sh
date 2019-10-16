862fe9c1f2cf3c3fb507af2ddee94b0d962010f0
boot1110001 <ppuubblliicc@protonmail.com>
2019-10-16 22:33:36 +0200

    todos los archivos1

--- install-server.sh
+++ install-server.sh
@@ -6,9 +6,9 @@ apt install openjdk-11-jre-headless curl screen nano bash grep git
 mkdir -p /opt/minecraft
 adduser --system --shell /bin/bash --home /opt/minecraft --group minecraft
 cd /opt/minecraft
-git clone https://github.com/boot1110001/minesrv.git
-cd /opt/minecraft/minesrv
+git clone https://github.com/boot1110001/familycraft.git
+cd /opt/minecraft/familycraft
 cp minecraft@.service /etc/systemd/system/minecraft@.service
 chown minecraft:minecraft -R /opt/minecraft #necesario
-systemctl enable minecraft@minesrv
-#systemctl disable minecraft@minesrv
+systemctl enable minecraft@familycraft
+#systemctl disable minecraft@familycraft
--- start.sh
+++ start.sh
@@ -2,3 +2,7 @@
 #java -Xms2G -Xmx2G -jar server.jar
 #java -Xms3737M -Xmx3737M -jar server.jar
 java -Xms3993M -Xmx3993M -jar server.jar
+
+# COSAS A PROBAR
+#/usr/bin/java -server -Xms16G -Xmx16G -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+AggressiveOpts -XX:+DisableExplicitGC -Xloggc:gc -jar server.jar nogui | tee ~/minecraft_server.log
+# 	(https://stackoverflow.com/questions/52239570/useconcmarksweepgc-is-deprecated-what-is-its-replacement)
